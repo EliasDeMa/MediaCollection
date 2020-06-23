62f4c235e7c9121fbaa13a2e163293d51d241426
Elias De Maertelaere <elias.demaertelaere@gmail.com>
2020-06-23 15:26:45 +0200

    Allow user to delete entry.

---
+++ MediaCollection/Models/MusicDeleteViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MediaCollection.Models
+{
+    public class MusicDeleteViewModel
+    {
+        public int Id { get; set; }
+        public string SongTitle { get; set; }
+        public string BandName { get; set; }
+    }
+}
