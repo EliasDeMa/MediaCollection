2bb2a84eb0db4eea25bb9f3a8bf980248ff77abd
Elias De Maertelaere <elias.demaertelaere@gmail.com>
2020-06-23 11:08:57 +0200

    Add basis for music controller.

---
+++ MediaCollection/Controllers/MusicController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MediaCollection.Data;
+using MediaCollection.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace MediaCollection.Controllers
+{
+    public class MusicController : Controller
+    {
+        private readonly ApplicationDbContext _applicationDbContext;
+
+        public MusicController(ApplicationDbContext applicationDbContext)
+        {
+            _applicationDbContext = applicationDbContext;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var songs = await _applicationDbContext.Songs
+                .Include(song => song.Album)
+                .ThenInclude(album => album.Band)
+                .ToListAsync();
+
+
+            return View(songs.Select(song => new MusicIndexViewModel { 
+                Id = song.Id,
+                SongTitle = song.Title,
+                BandName = song.Album.Band.Name,
+                AlbumTitle = song.Album.Title
+            }));
+        }
+
+        public IActionResult Create()
+        {
+            return View(new MusicCreateViewModel());
+        }
+    }
+}
