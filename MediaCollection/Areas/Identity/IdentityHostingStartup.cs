219207d9a20919b97e7be242e58bd9fa78f24d63
Elias De Maertelaere <elias.demaertelaere@gmail.com>
2020-06-23 10:37:15 +0200

    Scaffold login pages.

---
+++ MediaCollection/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using MediaCollection.Data;
+using MediaCollection.Domain;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(MediaCollection.Areas.Identity.IdentityHostingStartup))]
+namespace MediaCollection.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
--- MediaCollection/Startup.cs
+++ MediaCollection/Startup.cs
@@ -12,6 +12,7 @@ using MediaCollection.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MediaCollection.Domain;
 
 namespace MediaCollection
 {
@@ -30,7 +31,7 @@ namespace MediaCollection
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            services.AddDefaultIdentity<MediaCollectionUser>()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
             services.AddRazorPages();
