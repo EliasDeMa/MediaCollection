fdeb9444567177d16049f21d47457db40f85ac12
Elias De Maertelaere <elias.demaertelaere@gmail.com>
2020-06-25 13:24:26 +0200

    Rename classes to Song to indicate they influence onlt songs.

---
+++ MediaCollection/Models/SongViewModels/SongEditViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MediaCollection.Models
+{
+    public class SongEditViewModel
+    {
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Songtitle cannot be empty")]
+        public string SongTitle { get; set; }
+        public string AlbumTitle { get; set; }
+        public string BandName { get; set; }
+        public DateTime? ReleaseDate { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Duration cannot be zero")]
+        public TimeSpan Duration { get; set; }
+    }
+}
