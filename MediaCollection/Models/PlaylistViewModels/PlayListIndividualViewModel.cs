d5842d96075a14197454465b2bda45a41feb5763
Elias De Maertelaere <elias.demaertelaere@gmail.com>
2020-06-25 13:24:41 +0200

    Move playlist models to separate folder.

---
+++ MediaCollection/Models/PlaylistViewModels/PlayListIndividualViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace MediaCollection.Models
+{
+    public class PlayListIndividualViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ MediaCollection/Models/PlaylistViewModels/PlaylistIndexViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MediaCollection.Models
+{
+    public class PlaylistIndexViewModel
+    {
+        public string NewPlayListName { get; set; }
+        public IEnumerable<PlayListIndividualViewModel> PlayLists { get; set; }
+    }
+}
