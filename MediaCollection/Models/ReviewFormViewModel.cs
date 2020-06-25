f253da3c167ea96958d3dc430a0d85dcb93e94b3
Elias De Maertelaere <elias.demaertelaere@gmail.com>
2020-06-25 13:26:03 +0200

    Add review form model.

---
+++ MediaCollection/Models/ReviewFormViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MediaCollection.Models
+{
+    public class ReviewFormViewModel
+    {
+        public string NewReview { get; set; }
+        public int NewReviewScore { get; set; }
+    }
+}
