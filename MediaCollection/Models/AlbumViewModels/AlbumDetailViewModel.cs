fd9a27285f2632ecf4d3ea4b2d31f4857ca942a9
Elias De Maertelaere <elias.demaertelaere@gmail.com>
2020-06-25 13:25:25 +0200

    add folder for future album models.

---
+++ MediaCollection/Models/AlbumViewModels/AlbumDetailViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MediaCollection.Models
+{
+    public class AlbumDetailViewModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Band { get; set; }
+        public IEnumerable<(string, TimeSpan)> Songs { get; set; }
+    }
+}
