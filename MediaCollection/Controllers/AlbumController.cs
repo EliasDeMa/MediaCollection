6bc10762b5e8b4a777b3533cbf12624daa2beb01
Elias De Maertelaere <elias.demaertelaere@gmail.com>
2020-06-25 15:49:47 +0200

    Add album controller.

---
+++ MediaCollection/Controllers/AlbumController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MediaCollection.Data;
+using MediaCollection.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace MediaCollection.Controllers
+{
+    public class AlbumController : Controller
+    {
+        private readonly ApplicationDbContext _applicationDbContext;
+
+        public AlbumController(ApplicationDbContext applicationDbContext)
+        {
+            _applicationDbContext = applicationDbContext;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Detail(int id)
+        {
+            var album = await _applicationDbContext.Albums
+                .Include(album => album.Band)
+                .Include(album => album.Songs)
+                .FirstOrDefaultAsync(album => album.Id == id);
+
+            return View(new AlbumDetailViewModel
+            {
+                Id = album.Id,
+                Band = album.Band.Name,
+                Title = album.Title,
+                Songs = album.Songs.Select(song => (song.Title, song.Duration))
+            });
+        }
+    }
+}
