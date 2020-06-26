fde9183f642803e9b7b4ec4c19edbedbe6eecee6
Elias De Maertelaere <elias.demaertelaere@gmail.com>
2020-06-26 10:14:09 +0200

    move song related actions to song controller.

--- MediaCollection/Controllers/SongController.cs
+++ MediaCollection/Controllers/SongController.cs
@@ -60,6 +60,90 @@ namespace MediaCollection.Controllers
             return View(songModels);
         }
 
+        public IActionResult Create()
+        {
+            return View(new SongCreateViewModel());
+        }
+
+        [ValidateAntiForgeryToken]
+        [HttpPost]
+        public async Task<IActionResult> Create(SongCreateViewModel vm)
+        {
+            IQueryable<Band> bandExists = null;
+            IQueryable<Album> albumExists = null;
+
+            if (!string.IsNullOrEmpty(vm.AlbumTitle))
+            {
+                albumExists = _applicationDbContext.Albums.Where(album => album.NormalizedTitle == vm.AlbumTitle.ToUpper());
+            }
+
+            if (!string.IsNullOrEmpty(vm.BandName))
+            {
+                bandExists = _applicationDbContext.Bands.Where(band => band.NormalizedName == vm.BandName.ToUpper());
+            }
+
+            var songToDb = new Song
+            {
+                Title = vm.SongTitle,
+                NormalizedTitle = vm.SongTitle.ToUpper(),
+                Duration = vm.Duration
+            };
+
+            await ChangeAlbum(vm.AlbumTitle, albumExists, songToDb);
+
+            if (songToDb.Album != null && songToDb.Album.ReleaseDate == null && vm.ReleaseDate.HasValue)
+            {
+                songToDb.Album.ReleaseDate = vm.ReleaseDate.Value;
+            }
+
+            await ChangeBand(vm.BandName, bandExists, songToDb);
+
+            await _applicationDbContext.Songs.AddAsync(songToDb);
+            await _applicationDbContext.SaveChangesAsync();
+
+            return RedirectToAction("Index");
+        }
+
+        private static async Task ChangeBand(string bandName, IQueryable<Band> bandExists, Song songToDb)
+        {
+            if (bandExists == null)
+            {
+                return;
+            }
+            else if (bandExists.Any() && songToDb.Album.BandId == null)
+            {
+                songToDb.Album.Band = await bandExists.FirstOrDefaultAsync();
+            }
+            else if (!bandExists.Any())
+            {
+                songToDb.Album.Band = new Band
+                {
+                    Name = bandName,
+                    NormalizedName = bandName.ToUpper()
+                };
+            }
+        }
+
+        private static async Task ChangeAlbum(string albumTitle, IQueryable<Album> albumExists, Song songToDb)
+        {
+            if (albumExists == null)
+            {
+                return;
+            }
+            else if (albumExists.Any())
+            {
+                songToDb.Album = await albumExists.FirstOrDefaultAsync();
+            }
+            else
+            {
+                songToDb.Album = new Album
+                {
+                    Title = albumTitle,
+                    NormalizedTitle = albumTitle.ToUpper()
+                };
+            }
+        }
+
         public async Task<IActionResult> Detail(int id, bool alreadyReviewed = false)
         {
             var song = await _applicationDbContext.Songs
@@ -74,10 +158,10 @@ namespace MediaCollection.Controllers
                 Id = song.Id,
                 AlreadyReviewed = alreadyReviewed,
                 SongTitle = song.Title,
-                AlbumTitle = song.Album.Title,
-                BandName = song.Album.Band.Name,
+                AlbumTitle = song.Album?.Title,
+                BandName = song.Album?.Band?.Name,
                 Duration = song.Duration,
-                ReleaseDate = song.Album.ReleaseDate,
+                ReleaseDate = song.Album?.ReleaseDate,
                 Reviews = song.SongReviews.Select(review => new SongReviewViewModel
                 {
                     Description = review.Description,
@@ -100,6 +184,63 @@ namespace MediaCollection.Controllers
                     Id = item.Id,
                     Name = item.Name
                 });
+        }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            var song = await _applicationDbContext.Songs
+                .Include(song => song.Album)
+                .ThenInclude(album => album.Band)
+                .FirstOrDefaultAsync(item => item.Id == id);
+
+            return View(new SongEditViewModel
+            {
+                SongTitle = song.Title,
+                AlbumTitle = song.Album?.Title,
+                BandName = song.Album?.Band?.Name,
+                Duration = song.Duration,
+                ReleaseDate = song.Album?.ReleaseDate
+            });
+        }
+
+        [Authorize]
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, SongEditViewModel vm)
+        {
+            var bandExists = _applicationDbContext.Bands.Where(band => band.NormalizedName == vm.BandName.ToUpper());
+            var albumExists = _applicationDbContext.Albums.Where(album => album.NormalizedTitle == vm.AlbumTitle.ToUpper());
+            var origSong = await _applicationDbContext.Songs
+                .Include(song => song.Album)
+                .ThenInclude(album => album.Band)
+                .FirstOrDefaultAsync(item => item.Id == id);
+
+            origSong.Duration = vm.Duration;
+
+            if (origSong.NormalizedTitle != vm.SongTitle.ToUpper())
+            {
+                origSong.Title = vm.SongTitle;
+                origSong.NormalizedTitle = vm.SongTitle.ToUpper();
+            }
+
+            if (origSong.Album?.NormalizedTitle != vm.AlbumTitle)
+            {
+                await ChangeAlbum(vm.AlbumTitle, albumExists, origSong);
+                origSong.Album.ReleaseDate = vm.ReleaseDate;
+            }
+
+            if (!origSong.Album.ReleaseDate.HasValue)
+            {
+                origSong.Album.ReleaseDate = vm.ReleaseDate;
+            }
+
+            if (origSong.Album?.Band?.NormalizedName != vm.BandName)
+            {
+                await ChangeBand(vm.BandName, bandExists, origSong);
+            }
+
+            await _applicationDbContext.SaveChangesAsync();
+
+            return RedirectToAction("Detail", new { Id = id });
         }
     }
 }
