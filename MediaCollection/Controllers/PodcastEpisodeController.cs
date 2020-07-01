b0fd64067e5ec5f371f64e9cb328f1ecce12f5ef
Elias De Maertelaere <elias.demaertelaere@gmail.com>
2020-07-01 15:27:26 +0200

    Show individual podcast episodes.

---
+++ MediaCollection/Controllers/PodcastEpisodeController.cs
@@ -0,0 +1,35 @@
+﻿using MediaCollection.Data;
+using MediaCollection.Models.PodcastEpisodeModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MediaCollection.Controllers
+{
+    public class PodcastEpisodeController : Controller
+    {
+        private readonly ApplicationDbContext _applicationDbContext;
+
+        public PodcastEpisodeController(ApplicationDbContext applicationDbContext)
+        {
+            _applicationDbContext = applicationDbContext;
+        }
+
+        public async Task<IActionResult> Detail(int id)
+        {
+            var episode = await _applicationDbContext.PodcastEpisodes
+                .Include(ep => ep.Podcast)
+                .FirstOrDefaultAsync(ep => ep.Id == id);
+
+            return View(new PodcastEpisodeDetailViewModel
+            {
+                Link = episode.Link,
+                Duration = episode.Duration,
+                Title = episode.EpisodeTitle
+            });
+        }
+    }
+}
---
+++ MediaCollection/Models/PodcastEpisodeModels/PodcastEpisodeDetailViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MediaCollection.Models.PodcastEpisodeModels
+{
+    public class PodcastEpisodeDetailViewModel
+    {
+        public string Link { get; internal set; }
+        public TimeSpan Duration { get; internal set; }
+        public string Title { get; internal set; }
+    }
+}
