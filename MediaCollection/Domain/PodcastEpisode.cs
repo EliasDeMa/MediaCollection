50a808ce70757c0f6ce8f4fe64c292a702e503c0
Elias De Maertelaere <elias.demaertelaere@gmail.com>
2020-07-02 09:57:48 +0200

    Add episode number.

--- MediaCollection/Domain/PodcastEpisode.cs
+++ MediaCollection/Domain/PodcastEpisode.cs
@@ -8,6 +8,7 @@ namespace MediaCollection.Domain
         public int PodcastId { get; set; }
         public Podcast Podcast { get; set; }
         public string EpisodeTitle { get; set; }
+        public int EpisodeNumber { get; set; }
         public string NormalizedEpisodeTitle { get; set; }
         public TimeSpan Duration { get; set; }
         public string Link { get; set; }
