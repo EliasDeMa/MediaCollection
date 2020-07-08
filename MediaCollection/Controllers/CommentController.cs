477ea184cc91b9ca4b8752ac5b735920c2bf41a8
Elias De Maertelaere <elias.demaertelaere@gmail.com>
2020-07-08 15:27:16 +0200

    Add comment action.

---
+++ MediaCollection/Controllers/CommentController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using MediaCollection.Data;
+using MediaCollection.Domain;
+using MediaCollection.Models.PlaylistViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace MediaCollection.Controllers
+{
+    public class CommentController : Controller
+    {
+        private readonly ApplicationDbContext _applicationDbContext;
+
+        public CommentController(ApplicationDbContext applicationDbContext)
+        {
+            _applicationDbContext = applicationDbContext;
+        }
+
+        public async Task<IActionResult> AddComment(int id, AddPlaylistCommentViewModel vm)
+        {
+            var playlist = await _applicationDbContext.PlayLists
+                .FirstOrDefaultAsync(playlist => playlist.Id == id);
+
+            string userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            await _applicationDbContext.PlaylistComments.AddAsync(new PlaylistComment
+            {
+                UserId = userId,
+                Content = vm.Content,
+                PlaylistId = id
+            });
+
+            await _applicationDbContext.SaveChangesAsync();
+
+            return RedirectToAction("Playlist", "Detail", new { Id = id });
+        }
+    }
+}
