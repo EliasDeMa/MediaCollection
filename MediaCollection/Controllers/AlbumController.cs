2a5c5dd2701d9ea479fde8d2372b8c95d72d99c0
Elias De Maertelaere <elias.demaertelaere@gmail.com>
2020-07-08 13:04:08 +0200

    Be able to edit album reviews.

--- MediaCollection/Controllers/AlbumController.cs
+++ MediaCollection/Controllers/AlbumController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using MediaCollection.Data;
 using MediaCollection.Models;
+using MediaCollection.Models.AlbumViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -26,17 +27,51 @@ namespace MediaCollection.Controllers
         public async Task<IActionResult> Detail(int id)
         {
             var album = await _applicationDbContext.Albums
+                .Include(album => album.AlbumReviews)
+                .ThenInclude(albumReview => albumReview.User)
                 .Include(album => album.Band)
                 .Include(album => album.Songs)
                 .FirstOrDefaultAsync(album => album.Id == id);
 
-            return View(new AlbumDetailViewModel
+            var vm = new AlbumDetailViewModel
             {
                 Id = album.Id,
                 Band = album.Band.Name,
                 Title = album.Title,
                 Songs = album.Songs.Select(song => (song.Title, song.Duration))
-            });
+            };
+
+            var approvedReviews = new List<AlbumReviewViewModel>();
+
+            foreach (var item in album.AlbumReviews)
+            {
+                if (item.Approved)
+                {
+                    approvedReviews.Add(new AlbumReviewViewModel
+                    {
+                        Description = item.Description,
+                        Score = item.Score,
+                        User = item.User.UserName,
+                        Id = item.Id,
+                        Approved = true,
+                    });
+                }
+                else
+                {
+                    approvedReviews.Add(new AlbumReviewViewModel
+                    {
+                        Description = item.Description,
+                        Score = item.Score,
+                        User = item.User.UserName,
+                        Id = item.Id,
+                        Approved = false,
+                    });
+                }
+            }
+
+            vm.Reviews = approvedReviews;
+
+            return View(vm);
         }
     }
 }
