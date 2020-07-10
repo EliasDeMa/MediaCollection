ca0714f65b2d5d2f168cabdbcc0b952e61977548
Elias De Maertelaere <elias.demaertelaere@gmail.com>
2020-07-10 10:54:20 +0200

    Add model for song filter form and readjust existing models to accompany it.

---
+++ MediaCollection/Models/SongViewModels/SongFilterViewModel.cs
@@ -0,0 +1,17 @@
+﻿using MediaCollection.Data;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MediaCollection.Models.SongViewModels
+{
+    public class SongFilterViewModel
+    {
+        public int SelectedBand { get; set; }
+        public int SelectedAlbum { get; set; }
+        public IEnumerable<SelectListItem> BandNames { get; set; }
+        public IEnumerable<SelectListItem> AlbumTitles { get; set; }
+    }
+}
---
+++ MediaCollection/Models/SongViewModels/SongIndexViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MediaCollection.Models.SongViewModels
+{
+    public class SongIndexViewModel
+    {
+        public int Id { get; set; }
+        public int? AlbumId { get; set; }
+        public string SongTitle { get; set; }
+        public string BandName { get; set; }
+        public string AlbumTitle { get; set; }
+        public TimeSpan Duration { get; set; }
+        public DateTime? ReleaseDate { get; set; }
+        public bool Hidden { get; set; }
+    }
+}
