5685d62eacea7d6e015bb398fe814b51b08102a7
Elias De Maertelaere <elias.demaertelaere@gmail.com>
2020-07-13 13:17:56 +0200

    Show trending songs on home page.

--- MediaCollection/Controllers/HomeController.cs
+++ MediaCollection/Controllers/HomeController.cs
@@ -6,21 +6,76 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using MediaCollection.Models;
+using MediaCollection.Data;
+using Microsoft.EntityFrameworkCore;
+using MediaCollection.Models.HomeViewModels;
+using MediaCollection.Models.SongViewModels;
 
 namespace MediaCollection.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _applicationDbContext;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext applicationDbContext)
         {
             _logger = logger;
+            _applicationDbContext = applicationDbContext;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var topTenSongs = await _applicationDbContext.Songs
+                .Include(song => song.SongReviews)
+                .Include(song => song.Album)
+                .ThenInclude(song => song.Band)
+                .OrderByDescending(song => song.SongReviews.Count)
+                .Take(10)
+                .ToListAsync();
+
+            var songModels = new List<SongIndexViewModel>();
+
+            foreach (var song in topTenSongs)
+            {
+                if (song.Hidden)
+                {
+                    songModels.Add(new SongIndexViewModel
+                    {
+                        Hidden = true,
+                        Id = song.Id,
+                    });
+                }
+                else
+                {
+                    var model = new SongIndexViewModel
+                    {
+                        Id = song.Id,
+                        Hidden = false,
+                        SongTitle = song.Title,
+                        Duration = song.Duration,
+                    };
+
+                    if (song.Album != null)
+                    {
+                        model.AlbumId = song.AlbumId;
+                        model.AlbumTitle = song.Album.Title;
+                        if (song.Album.Band != null)
+                        {
+                            model.BandName = song.Album.Band.Name;
+                        }
+                    }
+
+                    songModels.Add(model);
+                }
+            }
+
+            var vm = new HomeIndexViewModel
+            {
+                topTenSongs = songModels
+            };
+
+            return View(vm);
         }
 
         public IActionResult Privacy()
---
+++ MediaCollection/Models/HomeViewModels/HomeIndexViewModel.cs
@@ -0,0 +1,11 @@
+﻿using MediaCollection.Domain;
+using MediaCollection.Models.SongViewModels;
+using System.Collections.Generic;
+
+namespace MediaCollection.Models.HomeViewModels
+{
+    public class HomeIndexViewModel
+    {
+        public IEnumerable<SongIndexViewModel> topTenSongs { get; set; }
+    }
+}
