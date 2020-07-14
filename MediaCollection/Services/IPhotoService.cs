fda6c877647a5b9eaba6c029874062e4c58b7260
Elias De Maertelaere <elias.demaertelaere@gmail.com>
2020-07-14 14:48:00 +0200

    Add photoservice.

---
+++ MediaCollection/Services/IPhotoService.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace MediaCollection.Services
+{
+    public interface IPhotoService
+    {
+        string AddPhoto(IFormFile photo);
+        void DeletePhoto(string fileName);
+    }
+}
